bb35cd703c1595bf086b98843834a36f27f9e89b
Tapi33 <artem8554134@gmail.com>
2022-07-03 13:51:33 +0300

    create axios request

--- src/pages/home/components/ThisDay/ThisDay.tsx
+++ src/pages/home/components/ThisDay/ThisDay.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import s from "./ThisDay.module.scss";
 import {GlobalSvgSelector} from "../../../../assets/icons/global/GlobalSvgSelector";
+import {Weather} from "../../../../store/types/types";
 
 
 interface Props {
+    weather:Weather
 }
-export const ThisDay = (props:Props) =>{
+export const ThisDay = ({weather}:Props) =>{
     return (
         <div className={s.this_day}>
             <div className={s.top_block}>
                 <div className={s.top_block_wrapper}>
-                    <div className={s.this_temp}>20°</div>
+                    <div className={s.this_temp}>{Math.round(weather.main.temp)}°</div>
                     <div className={s.this_day_name}>Сегодня</div>
                 </div>
                 <GlobalSvgSelector id={"sun"} />
